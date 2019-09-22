c02e6e99dc96a38bcd332509a82e350ac422fa47
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-22 15:31:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     false
-  elsif board[index].include?("X") || board[index].include?("O")
-    true 
+  elsif board[index] == "X" || board[index] == "O"
+    true
   end
 end
